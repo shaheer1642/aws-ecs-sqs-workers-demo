d2aeec22b677ca4696f8539fbe08a203e83e7777
shaheer1642 <shaheer1642@gmail.com>
2026-09-03 21:32:03 +0500

    feat: add api to post to sqs

---
+++ api/index.js
@@ -0,0 +1,33 @@
+require("dotenv").config();
+const express = require("express");
+const { SQSClient, SendMessageCommand } = require("@aws-sdk/client-sqs");
+
+const app = express();
+const sqs = new SQSClient({
+    region: "ap-south-1",
+    credentials: {
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_ACCESS_SECRET_KEY
+    }
+});
+
+const QUEUE_URL = process.env.QUEUE_URL;
+
+app.get("/send", async (req, res) => {
+    const message = req.query.message;
+
+    if (!message) {
+        return res.status(400).json({ error: "message is required" });
+    }
+
+    await sqs.send(new SendMessageCommand({
+        QueueUrl: QUEUE_URL,
+        MessageBody: JSON.stringify({ message })
+    }));
+
+    res.json({ success: true, message });
+});
+
+app.listen(3000, () => {
+    console.log("API running on port 3000");
+});
